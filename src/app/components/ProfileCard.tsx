0d57d68e514e8307e90b59ced66cfdef8a4c147b
Rabithua <1289770378@qq.com>
2025-06-17 17:52:36 +0800

    Increase Sticker image size in ProfileCard and remove unused ProfileCardNew component

--- src/app/components/ProfileCard.tsx
+++ src/app/components/ProfileCard.tsx
@@ -157,8 +157,8 @@ export default function ProfileCard({
             <Image
                 src="/Sticker.svg"
                 alt="Sticker"
-                width={250}
-                height={250}
+                width={500}
+                height={500}
                 className="size-48 z-1 absolute bottom-6 right-6"
             />
 
