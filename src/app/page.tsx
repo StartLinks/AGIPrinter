5013df91eeaf800d57c8c1acd6b6d3a3aed675a0
Rabithua <1289770378@qq.com>
2025-06-17 12:07:27 +0800

    更新页面布局，添加用户资料链接输入框，优化样式和结构

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 import useSWR from 'swr';
 import DraggableNote from "./components/DraggableNote";
 import { ProfileType } from "./type/profile";
@@ -19,6 +20,11 @@ const fetcher = async (url: string) => {
 };
 
 export default function Home() {
+  const [tags, settags] = useState<string[]>([
+    "Product Manager",
+    "Design",
+    "iOS Dev"
+  ]);
   // 使用SWR获取数据
   const { data, error, isLoading } = useSWR<ProfileType>(
     'https://fc-mp-b1a9bc8c-0aab-44ca-9af2-2bd604163a78.next.bspapp.com/profile/rabithua',
@@ -41,9 +47,22 @@ export default function Home() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className=" h-screen bg-gray-100 flex items-center justify-center gap-12">
+      <div className="flex flex-col border-2 divide-y-1 divide-black bg-gray-50 font-fusion-pixel">
+        <div className="flex flex-col gap-2 py-3  px-4"><div>专属链接</div>
+          <input
+            aria-label="Profile Link"
+            type="text"
+            value={data?.profile_link || "https://bonjour.bio/rabithua"}
+            readOnly
+            className="border border-black px-2 py-1 w-60"
+          />
+        </div>
+
+
+      </div>
       <div
-        className=" mx-auto aspect-[210/297] flex flex-col items-center gap-8 bg-white relative"
+        className=" aspect-[210/297] w-[595px] h-[842px] flex flex-col items-center gap-8 bg-white relative"
       >
         <Image
           src="/Menu bar.svg"
@@ -53,58 +72,55 @@ export default function Home() {
           className="w-full"
         />
 
-        <div className="flex gap-5 px-6 w-full">
-          <Image
-            src={data?.avatar || "https://cdn.bonjour.bio/static/image/defaultAvatar.svg"} // 使用SWR获取的头像或占位图
-            alt="Placeholder"
-            width={200}
-            height={200}
-            className="w-40 rounded-full border border-black"
-          />
-          <div className="flex flex-col justify-around">
-            <div className="text-5xl font-bold font-fusion-pixel">{data?.name}</div>
-            <div className="space-y-1">
-              {
-                (data?.basicInfo?.current_doing && data?.basicInfo?.role) && (
-                  <div className="text-3xl font-medium font-fusion-pixel">{
-                    `${data.basicInfo.current_doing}@${data.basicInfo.role}`
-                  }</div>)}
-              {data?.basicInfo?.region &&
-                Object.values(data.basicInfo.region).every(v => v !== undefined && v !== null && v !== "") && (
+        <div className="flex items-center gap-5 px-6 w-full">
+          <div className="flex gap-5 items-center">
+            <Image
+              src={data?.avatar || "https://cdn.bonjour.bio/static/image/defaultAvatar.svg"} // 使用SWR获取的头像或占位图
+              alt="Placeholder"
+              width={200}
+              height={200}
+              className="size-40 rounded-full shrink-0 border border-black"
+            />
+            <div className="flex flex-col gap-5 justify-around">
+              <div className="text-5xl font-bold font-fusion-pixel">{data?.name}</div>
+              <div className="space-y-1">
+                {
+                  (data?.basicInfo?.current_doing && data?.basicInfo?.role) && (
+                    <div className="text-3xl font-medium font-fusion-pixel">{
+                      `${data.basicInfo.current_doing}@${data.basicInfo.role}`
+                    }</div>)}
+                {data?.basicInfo?.region &&
+                  Object.values(data.basicInfo.region).every(v => v !== undefined && v !== null && v !== "") && (
+                    <div className="text-2xl font-medium font-fusion-pixel">
+                      {Object.values(data.basicInfo.region)
+                        .filter(v => v !== undefined && v !== null && v !== "")
+                        .join("，")}
+                    </div>
+                  )}
+                {data?.basicInfo?.gender && (
                   <div className="text-2xl font-medium font-fusion-pixel">
-                    {Object.values(data.basicInfo.region)
-                      .filter(v => v !== undefined && v !== null && v !== "")
-                      .join("，")}
+                    {data.basicInfo.gender}
                   </div>
                 )}
-              {data?.basicInfo?.gender && (
-                <div className="text-2xl font-medium font-fusion-pixel">
-                  {data.basicInfo.gender}
-                </div>
-              )}
+              </div>
             </div>
           </div>
-          <div className="ml-auto flex flex-col justify-around">
-            <div className="flex flex-col items-center">
-              <Image
-                src="/folder.svg"
-                alt="Folder"
-                width={50}
-                height={50}
-                className="w-12 h-12"
-              />
-              <div className="text-xl font-medium font-finders-keepers">Folder</div>
-            </div>
-            <div className="flex flex-col items-center">
-              <Image
-                src="/folder.svg"
-                alt="Folder"
-                width={50}
-                height={50}
-                className="w-12 h-12"
-              />
-              <div className="text-xl font-medium font-finders-keepers">Folder</div>
-            </div>
+
+          <div className="ml-auto flex flex-col shrink-0 justify-around">
+            {
+              tags.map((tag, index) => (<div key={
+                index
+              } className="flex flex-col items-center">
+                <Image
+                  src="/folder.svg"
+                  alt="Folder"
+                  width={50}
+                  height={50}
+                  className="w-12 h-12"
+                />
+                <div className="text-xl font-medium font-finders-keepers">{tag}</div>
+              </div>))
+            }
           </div>
         </div>
 
@@ -181,7 +197,7 @@ export default function Home() {
           alt="Sticker"
           width={250}
           height={250}
-          className="size-48 z-1 absolute bottom-12 right-12"
+          className="size-48 z-1 absolute bottom-6 right-6"
         />
 
         {/* note */}
